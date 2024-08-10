9c0c33715821db28364acbe9e59c5b6d68c845b9
Karma <ledombre@gmail.com>
2024-08-10 14:54:53 +0200

    chore: Update install_and_build scripts for elevated privileges

--- install_and_build/install.py
+++ install_and_build/install.py
@@ -1,12 +1,22 @@
 import os
 import sys
+import ctypes
+
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+if not is_admin():
+    # Re-run the script with elevated privileges
+    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, ' '.join(sys.argv), None, 1)
+    sys.exit()
 
 # Get the major and minor numbers of the current python version
-
-PYTHON_VER = dict( zip( ("major", "minor", "patch"), tuple( sys.version_info ) [ 0 : 3 ] ) )
+PYTHON_VER = dict(zip(("major", "minor", "patch"), tuple(sys.version_info)[0:3]))
 
 # If the python version is 3.12 or more, back out.
-
 if PYTHON_VER['major'] >= 3 and PYTHON_VER['minor'] >= 12:
     raise Exception("Your python version must not exceed 3.11 due to compatibility issues with bottle.py (https://github.com/bottlepy/bottle/issues/1430)")
 
--- install_and_build/replace_bottle.py
+++ install_and_build/replace_bottle.py
@@ -1,14 +1,24 @@
 import os
 import sys
 import shutil
+import ctypes
+
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+if not is_admin():
+    # Re-run the script with elevated privileges
+    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, ' '.join(sys.argv), None, 1)
+    sys.exit()
 
 # Get the current python's path. If you have multiple pythons, run this code with the python you wish to use.
-
 PYTHON_PATH = os.path.dirname(sys.executable)
 
 # Navigate to bottle.py in site packages
 BOTTLE_PATH = PYTHON_PATH + r'\Lib\site-packages\bottle.py'
 
 # Overwrite the current bottle.py with the bottle.py we have here.
-
-shutil.copy2( os.path.dirname(os.path.realpath(__file__)) + '\\bottle.py', BOTTLE_PATH)
+shutil.copy2(os.path.dirname(os.path.realpath(__file__)) + '\\bottle.py', BOTTLE_PATH)
